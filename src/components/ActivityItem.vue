1eac3096c5f7d96057ecf30b9c1c4a23e69a19d0
Grisho4ek <shgrt@ukr.net>
2019-05-22 00:59:24 +0300

    component ActivityItem created, css styles added

---
+++ src/components/ActivityItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <article class="post">
+    <h4>{{ activity.title }}</h4>
+    <div class="media">
+      <div class="media-left">
+        <p class="image is-32x32">
+          <img src="../assets/user.png" alt>
+        </p>
+      </div>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <a href="#">Filip Jerga</a>
+            updated {{ activity.updatedAt }} minutes ago &nbsp;
+          </p>
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ['activity']
+};
+</script>
+
+<style scoped>
+</style>
+
+
